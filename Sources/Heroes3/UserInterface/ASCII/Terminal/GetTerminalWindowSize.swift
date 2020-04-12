8012ec15663577d99b01a4e75b20fb58be4452ae
Alexander Cyon <alex.cyon@gmail.com>
2020-04-12 20:52:22 +0200

    removed non working tput based terminal size query.

--- Sources/Heroes3/UserInterface/ASCII/Terminal/GetTerminalWindowSize.swift
+++ Sources/Heroes3/UserInterface/ASCII/Terminal/GetTerminalWindowSize.swift
@@ -7,13 +7,9 @@
 
 import Foundation
 
-func getTerminalSize() -> (width: UInt16, height: UInt16) {
-    getTerminalSizeTput()
-}
-
 /// From SO: https://stackoverflow.com/a/47777085/1311272
 import Darwin
-func getTerminalSizeIoctl() -> (width: UInt16, height: UInt16) {
+func getTerminalSize() -> (width: UInt16, height: UInt16) {
     var w = winsize()
     guard ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == 0 else {
         fatalError("failed to read window size")
@@ -22,34 +18,4 @@ func getTerminalSizeIoctl() -> (width: UInt16, height: UInt16) {
         width: w.ws_col,
         height: w.ws_row
     )
-}
-
-
-import ShellOut
-
-func getTerminalSizeTput() -> (width: UInt16, height: UInt16) {
-    do {
-        let width = try getTerminalWindow(dimension: .width)
-        let height = try getTerminalWindow(dimension: .height)
-        return (width: width, height: height)
-    } catch {
-        fatalError("shellout failed, error: \(error)")
-    }
-}
-
-private enum Dimension {
-    case width, height
-}
-
-private func getTerminalWindow(dimension: Dimension) throws -> UInt16 {
-    let cmd: Tput = (dimension == .width) ? .getScreenWidth : .getScreenHeight
-    let string = try tput(command: cmd)
-    guard let int = UInt16(string) else {
-        throw ParseError.failedToParseIntFromString(string)
-    }
-    return int
-}
-
-public enum ParseError: Swift.Error, Equatable {
-    case failedToParseIntFromString(String)
 }
